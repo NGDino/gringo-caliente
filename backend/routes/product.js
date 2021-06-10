8cc1b44fffe74e49597de5463551b7def781d45e
Neil Dino <N.dino90@gmail.com>
2021-06-09 22:34:45 -0600

    update product works

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router();
 
-const {getProducts, newProduct, getSingleProduct} = require('../controllers/productControllers');
+const {getProducts, newProduct, getSingleProduct, updateProduct} = require('../controllers/productControllers');
 
 router.route('/products').get(getProducts);
-
-router.route('/product/new').post(newProduct)
-
 router.route('/product/:id').get(getSingleProduct)
+
+router.route('/admin/product/new').post(newProduct)
+router.route('/admin/product/:id').put(updateProduct)
+
 
 module.exports = router;
