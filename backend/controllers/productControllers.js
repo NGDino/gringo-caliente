ca77a49d394cc2f1040a7ca55a5d5747c49f39c1
Neil Dino <N.dino90@gmail.com>
2021-06-11 17:07:25 -0600

    search by name done

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -2,6 +2,8 @@ const Product = require('../models/Product');
 const ErrorHandler = require('../utils/errorHandler')
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
 
+const APIFeatures= require('../utils/apiFeatures')
+
 //Create new product =>  /api/v1/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
@@ -15,7 +17,11 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 // get all products
 exports.getProducts = catchAsyncErrors(async (req, res, next)=>{
-    const products = await Product.find()
+    
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+                .search()
+
+    const products = await apiFeatures.query;
 
     res.status(200).json({
         success: true,
