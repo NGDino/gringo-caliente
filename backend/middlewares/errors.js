10ad14f30e0d7fdd1f0784cadf860f76a26136b2
Neil Dino <N.dino90@gmail.com>
2021-06-11 14:05:38 -0600

    validation and mongoose errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -21,6 +21,18 @@ module.exports = (err, req, res, next) => {
 
         error.message = err.message;
 
+        //wrong mongoose object ipaddr
+        if(err.name === 'CastError'){
+            const message= `Resource not found. Invalid: ${err.path}`
+            error = new ErrorHandler(message, 400)
+        }
+
+        //handling mongoose validation error
+        if(err.name === 'ValidatorError'){
+            const message = Object.values(err.errors).map(value => value.message)
+            error= new ErrorHandler(message, 400)
+        }
+
         res.status(error.statusCode || 500).json({
             success: false,
             message: error.message || 'Internal Server Error'
