fb7d8c68e10282afdab72ab506f5fc6d5fcad1f8
Neil Dino <N.dino90@gmail.com>
2021-06-13 23:31:59 -0600

    duplicate email handler

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, 'Please enter your email'],
+        unique: true,
         validator: [validator.isEmail, 'Please enter valid email address']
     },
     password: {
