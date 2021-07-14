fdcfd5bfea51f53a3ad2d763f97126d5a677c9ba
Neil Dino <N.dino90@gmail.com>
2021-07-14 11:47:17 -0600

    footer header home basics:

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
+import Home from './components/Home';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
