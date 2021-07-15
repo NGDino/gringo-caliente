ee7b5f5edfada58e4dbd3ce984c23a69255e5fd4
Neil Dino <N.dino90@gmail.com>
2021-07-15 16:05:46 -0600

    added loader component

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -20,8 +20,8 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 // get all products
 exports.getProducts = catchAsyncErrors(async (req, res, next)=>{
 
-    const resPerPage = 2;
-    const productCount = await Product.countDocuments();
+    const resPerPage = 4;
+    const productsCount = await Product.countDocuments();
     
     const apiFeatures = new APIFeatures(Product.find(), req.query)
                 .search()
@@ -32,7 +32,7 @@ exports.getProducts = catchAsyncErrors(async (req, res, next)=>{
 
     res.status(200).json({
         success: true,
-        productCount,
+        productsCount,
         products
     })
 })
