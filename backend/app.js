9f657f2bfa5df506841c8f4b505097753fc0ed9c
Neil Dino <N.dino90@gmail.com>
2021-07-27 17:41:23 -0600

    add user upload cloudinary avatar

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const cloudinary = require('cloudinary');
+const fileUploader = require('express-fileupload');
 
 const dotenv = require('dotenv');
 
@@ -12,8 +12,9 @@ const errorMiddleware = require('./middlewares/errors')
 dotenv.config({ path: 'backend/config/config.env' })
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cookieParser())
+app.use(fileUploader());
 
 //impost all routes
 const products = require('./routes/product')
@@ -25,11 +26,6 @@ app.use('/api/v1', products)
 app.use('/api/v1', auth)
 app.use('/api/v1', order)
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
 
 // middleware for errors
 app.use(errorMiddleware)
