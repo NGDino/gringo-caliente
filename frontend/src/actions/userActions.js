643197998125a4ca5ac4fe7c5bd74df5c6317b9c
Neil Dino <N.dino90@gmail.com>
2021-07-28 10:32:14 -0600

    update profile reducers

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -8,6 +8,10 @@ import  {
     REGISTER_USER_FAIL,
     LOAD_USER_REQUEST,
     LOAD_USER_SUCCESS,
+    UPDATE_PROFILE_REQUEST,
+    UPDATE_PROFILE_SUCCESS,
+    UPDATE_PROFILE_RESET,
+    UPDATE_PROFILE_FAIL,
     LOAD_USER_FAIL,
     LOGOUT_SUCCESS,
     LOGOUT_FAIL,
@@ -97,6 +101,33 @@ export const logout =() => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: LOGOUT_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+export const updateProfile =(userData) => async (dispatch) => {
+    try{
+        dispatch({
+            type: UPDATE_PROFILE_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'content-type': 'miltipart/form-data'
+            }
+        }
+
+        const {data} = await axios.put('api/v1/me/update', userData, config);
+
+        dispatch({
+            type: UPDATE_PROFILE_SUCCESS,
+            payload: data.success
+        })
+
+    }catch (error) {
+        dispatch({
+            type: UPDATE_PROFILE_FAIL,
             payload: error.response.data.message
         })
     }
