eacade208e02b6520bf7bd994b832819316bca8e
Neil Dino <N.dino90@gmail.com>
2021-08-11 14:54:04 -0600

    wrong path

--- backend/app.js
+++ backend/app.js
@@ -29,14 +29,14 @@ app.use('/api/v1', payment)
 app.use('/api/v1', auth)
 app.use('/api/v1', order)
 
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
+app.use('/images', express.static(path.join(__dirname, '../frontend/images')));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 }
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 //testing stats
